e46676e45158df28a36ce65bd831856d3768b7a5
Irina Sorokina <irasoro402@yandex.ru>
2023-03-24 01:25:36 +0400

    run build

--- lib/components/InputTime.d.ts
+++ lib/components/InputTime.d.ts
@@ -3,7 +3,10 @@ import './InputValue.css';
 interface PropsInput {
     value: string;
     setValue: (newValue: string) => void;
-    color: string;
+    color?: string;
+    locale?: string;
+    isConvert?: boolean;
+    title?: string;
 }
 declare const InputTime: (props: PropsInput) => JSX.Element;
 export default InputTime;
